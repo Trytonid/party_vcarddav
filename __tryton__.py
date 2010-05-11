f0cece8407be2eb5c784ff8ff27109211f8a8539
Cédric Krier <ced@b2ck.com>
2010-05-11 19:47:05 +0200

    Prepare release 1.6.0

--- __tryton__.py
+++ __tryton__.py
@@ -7,7 +7,7 @@
     'name_es_CO' : 'vCardDAV de Compañía',
     'name_es_ES' : 'Tercero - vCardDAV',
     'name_fr_FR': 'Tiers - vCardDAV',
-    'version' : '1.5.0',
+    'version' : '1.6.0',
     'author' : 'B2CK',
     'email': 'info@b2ck.com',
     'website': 'http://www.tryton.org/',
