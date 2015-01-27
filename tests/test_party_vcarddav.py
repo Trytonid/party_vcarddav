679c5db0a41de311f669b96a9838e56c50a495a5
Cédric Krier <ced@b2ck.com>
2015-01-27 18:13:41 +0100

    Add check for default model access
    When group access exists, a default model access should be defined otherwise
    all access are given to non group users.
    
    issue4485
    review5941002

--- tests/test_party_vcarddav.py
+++ tests/test_party_vcarddav.py
@@ -2,20 +2,14 @@
 # this repository contains the full copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import (POOL, DB_NAME, USER, CONTEXT,
-    test_depends, test_menu_action)
+from trytond.tests.test_tryton import ModuleTestCase
+from trytond.tests.test_tryton import POOL, DB_NAME, USER, CONTEXT
 from trytond.transaction import Transaction
 
 
-class PartyVCardDAVTestCase(unittest.TestCase):
+class PartyVCardDAVTestCase(ModuleTestCase):
     'Test PartyVCardDAV module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('party_vcarddav')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'party_vcarddav'
 
     def test0010party_vcard_report(self):
         'Test Party VCARD report'
