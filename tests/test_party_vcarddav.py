a76232c72e86ddecc4c74b5429ee658295fcda58
brunnenschacht <kitpulap.talurs@gmail.com>
2016-08-26 21:03:15 +0200

    Add test for Address.vcard2values
    issue5773
    review27531002

--- tests/test_party_vcarddav.py
+++ tests/test_party_vcarddav.py
@@ -1,6 +1,7 @@
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 import unittest
+import vobject
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import ModuleTestCase, with_transaction
 from trytond.pool import Pool
@@ -23,6 +24,26 @@ class PartyVCardDAVTestCase(ModuleTestCase):
         oext, content, _, _ = VCardReport.execute([party1.id], {})
         self.assertEqual(oext, 'vcf')
         self.assertIn('FN:Party 1', str(content))
+
+    @with_transaction()
+    def test_address_vcard2values(self):
+        'Test Address.vcard2values'
+        pool = Pool()
+        Address = pool.get('party.address')
+
+        values = {
+            'street': '300 Cliff Street',
+            'city': 'Scranton',
+            'zip': '18503',
+            }
+
+        vcard = vobject.vCard()
+        vcard.add('ADR')
+        vcard.adr.type_param = 'HOME'
+        vcard.adr.value = vobject.vcard.Address(
+            street=values['street'], city=values['city'], code=values['zip'])
+
+        self.assertDictEqual(Address.vcard2values(vcard.adr), values)
 
 
 def suite():
